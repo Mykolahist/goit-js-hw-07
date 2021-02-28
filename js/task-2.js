7d7b7cf502f7577c0a8c2579956ddd74f90394f7
Mykolahist <71245049+Mykolahist@users.noreply.github.com>
2021-02-28 13:39:03 +0200

    task2

--- js/task-2.js
+++ js/task-2.js
@@ -1,1 +1,21 @@
-const elements = document.createElement();
+const ingredients = [
+  'Картошка',
+  'Грибы',
+  'Чеснок',
+  'Помидоры',
+  'Зелень',
+  'Приправы',
+];
+
+const scheduleIngredients = document.querySelector('#ingredients');
+
+const elements = ingredients.map(option => {
+     const ingrElements = document.createElement('li');
+    ingrElements.textContent = option;
+
+    return ingrElements;
+});
+
+console.log(elements);
+
+scheduleIngredients.append(...elements);
